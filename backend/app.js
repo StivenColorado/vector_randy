e9b0871540722effa193dc76c47fa36eef2519fe
Stiven Colorado <90488682+StivenColorado@users.noreply.github.com>
2024-09-19 15:45:22 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,20 @@
+como lo configuro para que coja el servidor:
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const db = require('./config/db.json');
 const path = require('path');
-
+const port = db.port;
+const host = db.host;
 const app = express();
 
-// Usa el puerto proporcionado por Railway o el puerto local para desarrollo
-const port = process.env.PORT || db.port;
-
 app.use(cors({
-  origin: 'https://vector-randy.vercel.app', // Ajusta este origen según tu frontend
+  origin: 'https://vector-randy.vercel.app',
   credentials: true
 }));
 
-// Middlewares
+// Resto de los middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/imagenes', express.static(path.join(__dirname, 'imagenes')));
@@ -27,7 +26,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}/api`);
+  console.log(Servidor escuchando en ${host}:${port}/api);
 });
